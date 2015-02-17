a99d886769a9da1d40ec4dd9d8d19d5c1d669f8b
Joonhwan Lee <joonhwan@gmail.com>
2015-02-17 21:10:11 +0900

    authentication setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+//= require materialize-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+	
+	$(document).ready(function(){
+		$(".button-collapse").sideNav();
+		$(".dropdown-button").dropdown({ hover: false });
+	});
